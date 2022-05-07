20d4aee9ccfce95e9caef1369d8ce415a3b5f954
gnejfejf2 <gnejfejf3@naver.com>
2022-05-07 12:56:05 +0900

    테스트

--- Targets/HelpMeChooseIOS/Tests/AppTests.swift
+++ Targets/HelpMeChooseIOS/Tests/AppTests.swift
@@ -5,4 +5,8 @@ final class HelpMeChooseIOSTests: XCTestCase {
     func test_twoPlusTwo_isFour() {
         XCTAssertEqual(2+2, 4)
     }
+    
+    func test_twoPlusTwo() {
+        XCTAssertEqual(2+2, 4)
+    }
 }
